f0601ebe22a6e12149946701c5585083dd6686d5
RuslanDMdev <91013665+RuslanDMdev@users.noreply.github.com>
2023-01-18 12:28:50 +0300

    delete storyboard

---
+++ instaApp/Feed/FeedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FeedViewController.swift
+//  instaApp
+//
+//  Created by Ruslan Dalgatov on 18.01.2023.
+//
+
+import SnapKit
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    //MARK: - View lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initialize ()
+
+    }
+}
+
+
+// MARK: - Private methods
+
+private extension FeedViewController {
+    func initialize(){
+        view.backgroundColor = .green
+
+    }
+}
