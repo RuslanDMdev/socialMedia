cc2a95daf99a277a776a1144a1f31faaba8ea90f
RuslanDMdev <91013665+RuslanDMdev@users.noreply.github.com>
2023-01-24 17:04:35 +0300

    navigation bar
    add logo, favs, subs, direct

--- instaApp/Feed/FeedViewController.swift
+++ instaApp/Feed/FeedViewController.swift
@@ -24,7 +24,39 @@ class FeedViewController: UIViewController {
 
 private extension FeedViewController {
     func initialize(){
-        view.backgroundColor = .green
-
+        view.backgroundColor = .white
+        navigationController?.navigationBar.tintColor = .black
+        navigationItem.rightBarButtonItems = makeRightBarButtonItems()
+        navigationItem.leftBarButtonItems = makeLeftBarButtonItems()
+    }
+    
+    func makeRightBarButtonItems() -> [UIBarButtonItem]{
+        let addBarButtunItem = UIBarButtonItem(title: "nil", image: UIImage(systemName: "plus"), target: self, action: #selector(didTapPlusButton))
+        let directBarButtunItem = UIBarButtonItem(title: "nil", image: UIImage(systemName: "paperplane"), target: self, action: #selector(didTapDirectButton))
+        return[directBarButtunItem, addBarButtunItem]
+    }
+    
+    @objc func didTapPlusButton(){
+        print("Add button tapped")
+    }
+    
+    @objc func didTapDirectButton(){
+        print("Direct button tapped")
+    }
+    
+    func makeLeftBarButtonItems() -> [UIBarButtonItem] {
+        let logoBarButtunItem = UIBarButtonItem(customView: LogoView())
+        let dropDownButtomItem = UIBarButtonItem(title: nil, image: UIImage(systemName: "chevron.down"), target: self, action: nil, menu: makeDrowDownMenu())
+    return [logoBarButtunItem, dropDownButtomItem]
+    }
+    
+    func makeDrowDownMenu() -> UIMenu{
+        let subItem = UIAction(title: "Подписки", image: UIImage(systemName: "person.2")) { _ in
+            print("Subs")
+        }
+        let favsItem = UIAction(title: "Избранное", image: UIImage(systemName: "star")) { _ in
+            print("Faforite")
+        }
+        return UIMenu(title: "", children: [subItem, favsItem])
     }
 }
---
+++ instaApp/Views/LogoView.swift
@@ -0,0 +1,41 @@
+//
+//  LogoView.swift
+//  instaApp
+//
+//  Created by Ruslan Dalgatov on 24.01.2023.
+//
+
+import SnapKit
+import UIKit
+
+class LogoView: UIView{
+    //MARK: - init
+    
+    init(){
+        super.init(frame: .zero)
+        inialize()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+// MARK: - Private properties
+    private let imageView: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(named: "Instagram_logo")
+        return view
+    }()
+
+// MARK: - Private methods
+
+private extension LogoView{
+    func inialize(){
+        addSubview(imageView)
+        imageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+            make.width.equalTo(104)
+            make.height.equalTo(40)
+        }
+    }
+}
