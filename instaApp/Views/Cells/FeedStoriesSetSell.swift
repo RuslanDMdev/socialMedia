0a9c8089001b5147e656db95636ceae5296e7887
RuslanDMdev <91013665+RuslanDMdev@users.noreply.github.com>
2023-02-17 09:36:29 +0300

    add carousel

--- instaApp/Views/Cells/FeedStoriesSetSell.swift
+++ instaApp/Views/Cells/FeedStoriesSetSell.swift
@@ -9,7 +9,50 @@ import UIKit
 import SnapKit
 
 class FeedStoriesSetSell: UITableViewCell {
+    //MARK: - Public
     func configure(with info: FeedStoriesCellInfo){
-        
+        self.items = info
+        collectionView.reloadData()
+    }
+    
+    //MARK: - Init
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        initialize()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    //MARK: - private properties
+    
+    private var collectionView: UICollectionView!
+    private var items: FeedStoriesCellInfo = []
+    
+}
+
+//MARK: - Private methods
+
+private extension FeedStoriesSetSell{
+    func initialize(){
+        let layout = UICollectionViewFlowLayout()
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(StoriesItemCell.self, forCellWithReuseIdentifier: String(describing: StoriesItemCell.self))
+        collectionView.dataSource = self
+    }
+    
+}
+
+//MARK: - UICollectionViewDataSource
+extension FeedStoriesSetSell: UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: StoriesItemCell.self), for: indexPath) as! StoriesItemCell
+        cell.configure(with: items[indexPath.item])
+        return cell
     }
 }
---
+++ instaApp/Views/Cells/StoriesItemCell.swift
@@ -0,0 +1,27 @@
+//
+//  File.swift
+//  instaApp
+//
+//  Created by Ruslan Dalgatov on 17.02.2023.
+//
+
+import SnapKit
+import UIKit
+
+class StoriesItemCell: UICollectionViewCell{
+    //MARK: - Public
+    func configure(with info: FeedStoriesItemCellInfo){
+        
+    }
+    //MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+//MARK: - Private methods
+
+
